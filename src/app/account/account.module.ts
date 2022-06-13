8a93df501f344fbbac1f910f97327c4d46f548e7
ManarYousry <77962166+ManarYousry@users.noreply.github.com>
2022-06-13 10:48:14 +0200

    new

--- src/app/account/account.module.ts
+++ src/app/account/account.module.ts
@@ -4,6 +4,13 @@ import { LoginComponent } from './login.component';
 import { RegisterComponent } from './register.component';
 import { AccountRoutingModule } from './account-routing.module';
 import { SharedModule } from '../shared/shared.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon';
 
 
 
@@ -15,7 +22,17 @@ import { SharedModule } from '../shared/shared.module';
   imports: [
     CommonModule,
     AccountRoutingModule,
-    SharedModule
+    SharedModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatCardModule,
+    MatTableModule,
+    MatButtonModule,
+   MatFormFieldModule,
+   MatInputModule,
+   MatIconModule
+   
+  
   ]
 })
 export class AccountModule { }
