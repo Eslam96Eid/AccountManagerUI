b514fa127c1b5056346cafff3d7078742d98f8b1
boles gamel <boles.gamel123@gmail.com>
2022-06-15 17:26:43 +0200

    first commit

--- src/app/account/account.service.ts
+++ src/app/account/account.service.ts
@@ -61,6 +61,7 @@ register(values: any) {
 
 logout() {
   localStorage.removeItem('token');
+  console.log("first commit")
   this.currentUserSource.next(null);
   this.router.navigateByUrl('/');
 }
